a4d0c73d1186ee238f988d8736166dba0f0fe971
Mathias Hoeld <mathiashoeld@users.noreply.github.com>
2022-04-19 13:27:19 +0200

    Add flake8 for linting

--- api/webserver.py
+++ api/webserver.py
@@ -25,29 +25,19 @@ async def health():
 
 @app.get("/credentials")
 def get_credentials():
-    # return read_credentials()
     return 200
 
 
 @app.get("/experiment")
-def get_named_experiment_result(risk_weight:float=1, esg_weight:float=1, job_name:str="osqp"):
-    # return util.post_job_get_results(float(risk_weight), float(esg_weight), job_name)
-    return 200
+def get_named_experiment_result(risk_weight: float = 1, esg_weight: float = 1, job_name: str = "osqp"):
+    return {"riskWeight": risk_weight, "esgWeight": esg_weight, "jobName": job_name}
 
 
 @app.get("/timeseries/{ticker_symbol}")
 def get_time_series(ticker_symbol: str):
-    # ticker, stock_values = util.get_time_series(ticker_symbol)
-    # if ticker is None:
-    #     return {"error": "No data found for ticker symbol: " + ticker_symbol}
-    # return {'tickerSymbol': ticker, 'values': stock_values}
-    return 200
+    return {"tickerSymbol": ticker_symbol}
 
 
 @app.get("/available_tickers")
 def get_available_tickers():
-    # stock_items = util.get_available_stock_tickers()
-    # return {"availableTickers": [{"ticker": i[0], "fullName": i[1]} for i in stock_items if i != "Account"]}
     return 200
-
-
