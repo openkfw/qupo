bea1cdd8d3805308e2bf404f8a7ba3579aa03104
melissa.schmidt <melissa.schmidt@accenture.com>
2022-05-16 15:38:27 +0200

    Changes in tickers.py

--- api/app/tickers/tickers.py
+++ api/app/tickers/tickers.py
@@ -1,25 +1,25 @@
 from fastapi import APIRouter
 from stocksymbol import StockSymbol
 
+from ..config import settings
+
 router = APIRouter(
     prefix='/tickers',
     tags=['tickers'],
 )
 
-api_key_stock_symbol = ""
+api_key_stock_symbol = settings.stock_symbol_api_key
 stock_symbol = StockSymbol(api_key_stock_symbol)
+
 
 @router.get('', tags=['tickers'])
 async def get_all_tickers():
     return [{'name': 'ticker1'}, {'name': 'ticker2'}]
 
-@router.get("/markets")
-async def getMarkets():
-    return stock_symbol.market_list
 
 @router.get("/symbols")
 @router.get("/symbols/{symbols_only}")
-async def getSymbols(symbols_only = False):
+async def get_symbols(symbols_only=False):
     markets = stock_symbol.market_list
     symbols = []
 
@@ -28,6 +28,12 @@ async def getSymbols(symbols_only = False):
 
     return symbols
 
+
+@router.get("/markets")
+async def get_markets():
+    return stock_symbol.market_list
+
+
 @router.get("/symbols/{market}")
-async def getSymbols(market):
+async def get_symbols_of_market(market):
     return stock_symbol.get_symbol_list(market=market)
