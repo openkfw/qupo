eb48fb22f4573d21dfa05dbc0043cd0751383cc2
melissa.schmidt <melissa.schmidt@accenture.com>
2022-05-17 11:43:18 +0200

    Add endpoint to list symbols by index

--- api/app/tickers/tickers.py
+++ api/app/tickers/tickers.py
@@ -1,3 +1,4 @@
+from xmlrpc.client import boolean
 from fastapi import APIRouter
 from stocksymbol import StockSymbol
 
@@ -17,9 +18,8 @@ async def get_all_tickers():
     return [{'name': 'ticker1'}, {'name': 'ticker2'}]
 
 
-@router.get("/symbols")
-@router.get("/symbols/{symbols_only}")
-async def get_symbols(symbols_only=False):
+@router.get("/symbols/")
+async def get_symbols(symbols_only: boolean = False):
     markets = stock_symbol.market_list
     symbols = []
 
@@ -34,6 +34,16 @@ async def get_markets():
     return stock_symbol.market_list
 
 
-@router.get("/symbols/{market}")
+@router.get("/markets/{market}")
 async def get_symbols_of_market(market):
     return stock_symbol.get_symbol_list(market=market)
+
+
+@router.get("/indices")
+async def get_indices():
+    return stock_symbol.index_list
+
+
+@router.get("/indices/{index}")
+async def get_symbols_of_index(index):
+    return stock_symbol.get_symbol_list(index=index)
